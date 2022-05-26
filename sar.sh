1b6fec80cab3aedc5f48866c1423b60fc3e249cb
a-iftikhar <aiftikhar@nisum.com>
2022-05-26 14:00:10 +0000

    initial commit

---
+++ sar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#instalations first 
+
+sudo apt install sysstat -y
+sudo apt install sshpass
+
+#check process and store it in sar command
+
+sar 1 6 >> sar.txt
+
+#secure copy command to send file to nisum vm
+
+sshpass -p 'nisum' scp "sar.txt" nisum@192.168.6.129:/home/nisum/
+
